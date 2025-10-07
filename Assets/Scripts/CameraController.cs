71dd433bf2d6f2c6fc37e27f7d2620d8f54f3499
SeregaKazak <seregakazak2020@gmail.com>
2025-10-07 13:46:59 +0300

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float sensitivity = 2.0f;
+    public float maxYAngle = 80.0f;
+
+    public float rotationX = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.parent.Rotate(Vector3.up * mouseX * sensitivity);
+
+        rotationX -= mouseY * sensitivity;
+        rotationX = Mathf.Clamp(rotationX, -maxYAngle, maxYAngle);
+        transform.localRotation = Quaternion.Euler(rotationX, 0.0f, 0.0f);
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+    private CharacterController controller;
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.forward * verticalInput + transform.right* horizontalInput;
+
+        moveDirection.y -= 9.81f * Time.deltaTime;
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+    }
+
+}
